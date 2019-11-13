583d20c1c5cec3b058607ed8ec7baff06aaec0ee
Doug Kintop <dkintop@hotmail.com>
2019-11-13 15:46:22 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist 
   belongs_to :genre
   has_many :notes
-
+accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
